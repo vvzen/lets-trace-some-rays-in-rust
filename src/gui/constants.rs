6e7eea77e0628fbca11c76b99c2ca6cb4d6be7bb
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2023-03-04 17:07:53 +0000

    feat: Bump up resolution

--- src/gui/constants.rs
+++ src/gui/constants.rs
@@ -1,8 +1,8 @@
 pub const WINDOW_WIDTH: u32 = 1500;
 pub const WINDOW_HEIGHT: u32 = 720;
 
-pub const RENDER_BUFFER_WIDTH: u32 = 256;
-pub const RENDER_BUFFER_HEIGHT: u32 = 256;
+pub const RENDER_BUFFER_WIDTH: u32 = 1024;
+pub const RENDER_BUFFER_HEIGHT: u32 = 1024;
 pub const RENDER_BUFFER_SIZE: usize = (RENDER_BUFFER_WIDTH * RENDER_BUFFER_HEIGHT * 4) as usize;
 
 // 1024 * 1024 * 4         = 4_194_304
